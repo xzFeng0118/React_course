b81fd9606e6b6aecf81a750f7082bc5010632183
Xuanzhe Feng <xzfeng0118@gmail.com>
2023-10-12 21:56:42 +0800

    Update for new version react

---
+++ react_course/src/index.js
@@ -0,0 +1,8 @@
+//console.log("Hello World!");
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
+
+const container=document.getElementById("root")
+const root=createRoot(container)
+root.render(<h1>111</h1>)
